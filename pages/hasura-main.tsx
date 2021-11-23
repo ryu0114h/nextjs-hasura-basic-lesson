3dda4dd73cef1a5f0e9b0fcaa0d06fc053bb3b75
星野龍 <ryu0114h@gmail.com>
2021-11-23 15:43:23 +0900

    Direct access to cache (@client)

--- pages/hasura-main.tsx
+++ pages/hasura-main.tsx
@@ -7,8 +7,8 @@ import { Layout } from '../components/Layout'
 
 const FetchMain: VFC = () => {
   const { data, error } = useQuery<GetUsersQuery>(GET_USERS, {
-    //fetchPolicy: 'network-only',
-    fetchPolicy: 'cache-and-network',
+    fetchPolicy: 'network-only',
+    // fetchPolicy: 'cache-and-network',
     //fetchPolicy: 'cache-first',
     //fetchPolicy: 'no-cache',
   })
